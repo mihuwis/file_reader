df95400cda0aba140687267398c56120f8426ad1
Michal Wisniewski <mihuwis@gmail.com>
2019-06-02 17:15:17 +0200

    Add Who can Into exercise. Using streams to filter objects on given criteria

--- src/main/java/com/mihuwis/file_reader/exercises/whoCanIntoExercise/Person.java
+++ src/main/java/com/mihuwis/file_reader/exercises/whoCanIntoExercise/Person.java
@@ -1,4 +1,30 @@
 package com.mihuwis.file_reader.exercises.whoCanIntoExercise;
 
+import java.util.List;
+
 public class Person {
+    private String name;
+    private String phone;
+    private List<String> skills;
+
+    public Person(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public List<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skill) {
+        this.skills.add(skill);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
